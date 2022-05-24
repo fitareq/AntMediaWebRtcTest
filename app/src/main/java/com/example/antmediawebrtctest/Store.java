9983fd1280e223bb085180e261d29c60d5584d98
Forhadul Islam <fitareq@gmail.com>
2022-05-24 17:18:19 +0600

    initial commit

--- app/src/main/java/com/example/antmediawebrtctest/Store.java
+++ app/src/main/java/com/example/antmediawebrtctest/Store.java
@@ -1,4 +1,183 @@
 package com.example.antmediawebrtctest;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Store {
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("email")
+    @Expose
+    private Object email;
+    @SerializedName("phone")
+    @Expose
+    private Object phone;
+    @SerializedName("address")
+    @Expose
+    private String address;
+    @SerializedName("country")
+    @Expose
+    private String country;
+    @SerializedName("state")
+    @Expose
+    private String state;
+    @SerializedName("city")
+    @Expose
+    private String city;
+    @SerializedName("customer_id")
+    @Expose
+    private Integer customerId;
+    @SerializedName("logo")
+    @Expose
+    private String logo;
+    @SerializedName("description")
+    @Expose
+    private Object description;
+    @SerializedName("content")
+    @Expose
+    private Object content;
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("vendor_verified_at")
+    @Expose
+    private Object vendorVerifiedAt;
+    @SerializedName("created_at")
+    @Expose
+    private String createdAt;
+    @SerializedName("updated_at")
+    @Expose
+    private String updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getEmail() {
+        return email;
+    }
+
+    public void setEmail(Object email) {
+        this.email = email;
+    }
+
+    public Object getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Object phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public Object getDescription() {
+        return description;
+    }
+
+    public void setDescription(Object description) {
+        this.description = description;
+    }
+
+    public Object getContent() {
+        return content;
+    }
+
+    public void setContent(Object content) {
+        this.content = content;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Object getVendorVerifiedAt() {
+        return vendorVerifiedAt;
+    }
+
+    public void setVendorVerifiedAt(Object vendorVerifiedAt) {
+        this.vendorVerifiedAt = vendorVerifiedAt;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
--- app/src/main/java/com/example/antmediawebrtctest/Vendor.java
+++ app/src/main/java/com/example/antmediawebrtctest/Vendor.java
@@ -1,5 +1,53 @@
 package com.example.antmediawebrtctest;
 
-public class Vendor {
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
+public class Vendor {
+    @SerializedName("access_token")
+    @Expose
+    private String accessToken;
+    @SerializedName("token_type")
+    @Expose
+    private String tokenType;
+    @SerializedName("expires_in")
+    @Expose
+    private Integer expiresIn;
+    @SerializedName("store")
+    @Expose
+    private List<Store> store;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public Integer getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Integer expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public List<Store> getStore() {
+        return store;
+    }
+
+    public void setStore(List<Store> store) {
+        this.store = store;
+    }
 }
