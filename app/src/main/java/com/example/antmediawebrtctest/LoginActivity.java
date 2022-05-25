4e05d2c5f95073e7777f5b5e347418e3f6f80025
Forhadul Islam <fitareq@gmail.com>
2022-05-25 11:58:33 +0600

    initial commit

--- app/src/main/java/com/example/antmediawebrtctest/LoginActivity.java
+++ app/src/main/java/com/example/antmediawebrtctest/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.antmediawebrtctest;
 import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -26,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputEditText phone, password;
     private Button login;
     private RequestQueue requestQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,28 +38,36 @@ public class LoginActivity extends AppCompatActivity {
         phone = findViewById(R.id.login_phone);
         password = findViewById(R.id.login_password);
         login = findViewById(R.id.login);
-        login.setOnClickListener(view -> vendorLogin());
+        login.setOnClickListener(view -> {
+            login.setEnabled(false);
+            vendorLogin();
+        });
     }
 
-    private void vendorLogin(){
+    private void vendorLogin() {
+
         String _phone = phone.getText().toString();
         String _password = password.getText().toString();
         String url = "http://159.223.58.172/api/v1/vendor-login";
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url, response -> {
+            Log.v("api", "response: " + response);
             Gson gson = new Gson();
             Vendor vendor = gson.fromJson(response, Vendor.class);
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            intent.putExtra("name",vendor.getStore().get(0).getName() );
+            intent.putExtra("name", vendor.getStore().get(0).getName());
             intent.putExtra("logo", vendor.getStore().get(0).getLogo());
+            startActivity(intent);
         }, error -> {
+            Log.v("api", "error: " + error);
             Toast.makeText(this, "Something went wrong!!", Toast.LENGTH_LONG).show();
-        }){
+            login.setEnabled(true);
+        }) {
 
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<>();
-                params.put("email",_phone);
-                params.put("password",_password);
+                params.put("email", _phone);
+                params.put("password", _password);
 
                 return params;
             }
